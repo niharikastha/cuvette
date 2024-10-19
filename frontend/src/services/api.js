3884ba5306de69ecf3b26b7261c52a7dddec6327
astha niharika <astha.niharika@hyscaler.com>
2024-10-19 22:01:14 +0530

    removed unwanted

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,22 +1,10 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://cuvette-omega-silk.vercel.app/?vercelToolbarCode=qjaMRZnONDF9ODI', 
+  baseURL: 'https://cuvette-omega-silk.vercel.app', 
   withCredentials: true, 
 });
 
 axiosInstance.defaults.withCredentials = true;
-
-axiosInstance.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
-
 
 export default axiosInstance;
