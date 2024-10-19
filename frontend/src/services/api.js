f55a4f45218249b5ebda8b26e15de0750ac30113
astha niharika <astha.niharika@hyscaler.com>
2024-10-19 20:05:12 +0530

    require

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 const axiosInstance = axios.create({
-  // baseURL: 'https://trello-backend-vert.vercel.app/api', 
-  baseURL: 'http://localhost:5000', 
+  baseURL: 'https://cuvette-omega-silk.vercel.app', 
+  // baseURL: 'http://localhost:5000', 
   withCredentials: true, 
 });
 
